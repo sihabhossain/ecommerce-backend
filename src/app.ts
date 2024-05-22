7ea173eb8da76fc8de91a0a35adbf144443e4be9
Sihab Hossain <sihabhossain380@gmail.com>
2024-05-22 11:23:38 +0600

    last checkup

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use("/api/products", ProductRoutes);
 app.use("/api/orders", OrderRoutes);
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+  res.send("Welcome to the project management app");
 });
 
 export default app;
